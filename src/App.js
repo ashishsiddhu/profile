54d2708717c2dc036f8537fc35a11298cd369fcf
ashishsiddhu <32830765+ashishsiddhu@users.noreply.github.com>
2024-11-08 13:21:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
@@ -9,6 +9,7 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import CNCFrame from './Components/CNCFrame';
 
 class App extends Component {
 
@@ -16,7 +17,8 @@ class App extends Component {
     super(props);
     this.state = {
       foo: 'bar',
-      resumeData: {}
+      resumeData: {},
+      currentPath: window.location.hash,  // Set initial state based on URL hash
     };
 
     ReactGA.initialize('UA-110570651-1');
@@ -39,18 +41,39 @@ class App extends Component {
 
   componentDidMount(){
     this.getResumeData();
+    // Listen for hash changes in the URL
+    window.addEventListener('hashchange', this.handlePathChange);
   }
 
+  componentWillUnmount() {
+    // Clean up the event listener when the component is unmounted
+    window.removeEventListener('hashchange', this.handlePathChange);
+  }
+  handlePathChange = () => {
+    // Update the state to re-render the component when hash changes
+    this.setState({ currentPath: window.location.hash });
+  };
+
+
+  
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        {/* <Portfolio data={this.state.resumeData.portfolio}/> */}
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
+
+        {this.state.currentPath === '#contact' ?
+          <CNCFrame /> :
+          <div>
+            <Header data={this.state.resumeData.main} />
+            <About data={this.state.resumeData.main} />
+            <Resume data={this.state.resumeData.resume} />
+            {/* <Portfolio data={this.state.resumeData.portfolio}/> */}
+            <Testimonials data={this.state.resumeData.testimonials} />
+            <Contact data={this.state.resumeData.main} />
+            <Footer data={this.state.resumeData.main} />
+          </div>
+        }
+
+
       </div>
     );
   }
